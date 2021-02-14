c02819e41a5624a04fb1c71146fdc8289a6dcd31
Jose Valencia <sejoma989@gmail.com>
2021-02-13 21:39:53 -0500

    Cleaning pre defined files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world from React</div>
+  return <div className="App">
+    <div className="AddingPassword">
+      <input type="text" placeholder="Ej. Facebook" />
+      <input type="text" placeholder="Ej. www.facebook.com" />
+      <input type="text" placeholder="Ej. pepito@yimail.com" />
+      <input type="text" placeholder="Ej. password123" />
+      <button>Add password</button>
+    </div>
+  </div>
 }
 
 export default App;
