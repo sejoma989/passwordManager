c04f2c7683a4306f3a8897c3c18d533bcd2e15c7
Jose Valencia <sejoma989@gmail.com>
2021-02-20 01:56:11 -0500

    Creacion del moduilo de desencripcion

--- server/EncryptionManager.js
+++ server/EncryptionManager.js
@@ -3,7 +3,11 @@ const secret = '77Dios7777Dios7777Dios7777Dios77'
 
 const encrypt = (password) => {
     const iv = Buffer.from(crypto.randomBytes(16));
-    const cipher = crypto.createCipheriv('aes-256-ctr', Buffer.from(secret), iv);
+    const cipher = crypto.createCipheriv(
+        'aes-256-ctr', 
+        Buffer.from(secret), 
+        iv)
+        ;
 
     const encryptedPassword = Buffer.concat([
         cipher.update(password),
@@ -17,7 +21,18 @@ const encrypt = (password) => {
 };
 
 const decrypt = (encryption) => {
+    const decipher = crypto.createDecipheriv(
+        "aes-256-ctr",
+        Buffer.from(secret),
+        Buffer.from(encryption.iv, "hex")
+    );
 
+    const decryptedPassword = Buffer.concat([
+        decipher.update(Buffer.from(encryption.password, "hex")),
+        decipher.final(),
+    ]);
+
+    return decryptedPassword.toString("hex")
 };
 
 module.exports = {encrypt, decrypt};
